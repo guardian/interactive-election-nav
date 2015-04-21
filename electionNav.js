b26216af971bf2311a90d0ca163200e7280f7b5f
pomme <apple.chan-fardel@guardian.co.uk>
2015-04-21 17:24:35 +0100

    change based on Troy's VQA

--- electionNav.js
+++ electionNav.js
@@ -1,11 +1,13 @@
 var currentPath;
 
 var head = document.querySelector('head');
-var link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('type', 'text/css');
-    link.setAttribute('href', 'http://interactive.guim.co.uk/2015/04/election-nav/style.css');
-    // link.setAttribute('href', 'http://interactive.guim.co.uk/2015/04/election-nav/style.css');
+var link = document.createElement('link'),
+    linkDisabled = 'javascript: void(0)';
+
+link.setAttribute('rel', 'stylesheet');
+link.setAttribute('type', 'text/css');
+link.setAttribute('href', 'http://interactive.guim.co.uk/2015/04/election-nav/electionNav.css');
+//link.setAttribute('href', 'style.css'); //test
 
 head.appendChild(link);
 
@@ -26,10 +28,19 @@ var links = {
     }
 };
 
-function initElectionNav(pageId){
-    currentPath = pageId;
-    renderHead();
-    renderFooter();
+function createLink(txt, url, cn) {
+    // "p"arent node, "t"ext note, "a" element
+    var p = document.createElement('div'),
+        t = document.createTextNode(txt),
+        a = document.createElement('a');
+
+    a.appendChild(t);
+    a.href = url;
+    
+    p.appendChild(a);
+    p.className = cn; 
+    
+    return p;
 }
 
 function renderHead(){
@@ -38,30 +49,30 @@ function renderHead(){
 
     var innerContainer = document.createElement('div');
         innerContainer.setAttribute('class','electionNav-inner-container');
-
-    var headTitleContainer = document.createElement('div');
-        headTitleContainer.setAttribute('class','electionNav-head-title')
-        headTitleContainer.innerHTML = "<p>Election 2015</p>"
     
+    // Add head title
+    var headTitleContainer = createLink(
+        'Election 2015',
+        'http://www.theguardian.com/politics/general-election-2015',
+        'electionNav-head-title' 
+    );
     innerContainer.appendChild(headTitleContainer);
 
+    // Add head items
     for (var key in links){
-        var headItem = document.createElement('div');
-        var link = document.createElement('a');
-            link.setAttribute('href',links[key].url);
-            link.setAttribute('title',links[key].headline);
-            link.innerHTML = links[key].title;
-
-        if(key === currentPath){
-            headItem.setAttribute('class','electionNav-head-item electionNav-head-item-active')
-        }else{
-            headItem.setAttribute('class','electionNav-head-item');
+        var headItem, 
+            url = links[key].url, 
+            cn = 'electionNav-head-item'; 
+                
+        if (key === currentPath) {
+            // disable the active link
+            url = linkDisabled;
+            cn += ' electionNav-head-item-active'; 
         }
-        
-        headItem.appendChild(link);
+
+        headItem = createLink(links[key].title, url, cn);
         innerContainer.appendChild(headItem);
     }
-
     headContainer.appendChild(innerContainer);
 
     var headPlaceholder = document.querySelector('#election-nav-head');
@@ -75,28 +86,32 @@ function renderFooter(){
     var innerContainer = document.createElement('div');
         innerContainer.setAttribute('class','electionNav-inner-container');
 
-    var footerTitle = document.createElement('p');
-        footerTitle.setAttribute('class','electionNav-footer-title')
-        footerTitle.innerHTML = "Election 2015";
-
+    // Add footer title
+    var footerTitle = createLink(
+        'Election 2015',
+        'http://www.theguardian.com/politics/general-election-2015',
+        'electionNav-footer-title' 
+    );
     innerContainer.appendChild(footerTitle);
 
+    // Add footer items
     for (var key in links){
         var footerItem = document.createElement('div');
 
         var linkImage = document.createElement('img');
             linkImage.setAttribute('src',links[key].img);
-            linkImage.setAttribute('alt',links[key].title)
+            linkImage.setAttribute('alt',links[key].title);
 
         var link = document.createElement('a');
-            link.setAttribute('href', links[key].url);
             link.setAttribute('title',links[key].headline);
             link.innerHTML = "<span>" + links[key].title + "</span>" + linkImage.outerHTML;
 
         if(key === currentPath){
-            footerItem.setAttribute('class','electionNav-footer-item electionNav-footer-item-active')
+            footerItem.setAttribute('class','electionNav-footer-item electionNav-footer-item-active');
+            link.setAttribute('href', linkDisabled);
         }else{
             footerItem.setAttribute('class','electionNav-footer-item');
+            link.setAttribute('href', links[key].url);
         }
         
         footerItem.appendChild(link);
@@ -110,6 +125,14 @@ function renderFooter(){
     footerPlaceholder.innerHTML = footerContainer.outerHTML;
 }
 
+
 var pageId = window.guardian ? window.guardian.config.page.pageCode : undefined;
+
+function initElectionNav(pageId){
+    //currentPath = "2272795"; //test
+    currentPath = pageId;
+    renderHead();
+    renderFooter();
+}
 
 initElectionNav(pageId);
